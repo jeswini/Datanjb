2b85a5b8f740dce018981ba6b30fc072176e45a1
ogjes <o.g.jeswini@gmail.com>
2023-05-20 10:23:16 -0400

    third dbcknd

--- DataSourceDjangoAPI/settings.py
+++ DataSourceDjangoAPI/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['https://datasourcenj.herokuapp.com/', ' http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['datasourcenj.herokuapp.com/', '127.0.0.1:8000/']
 
 
 # Application definition
